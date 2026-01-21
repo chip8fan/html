5b06edfe92e916de6b311357aae7fa861aa5230b
chip8fan <nguyennamtran2022@gmail.com>
2026-01-21 17:03:55 -0600

    update client

---
+++ chess/assign_black.php
@@ -0,0 +1,3 @@
+<?php
+	file_put_contents("black.txt", file_get_contents("php://input"));
+?>
--- chess/client.py
+++ chess/client.py
@@ -1,15 +1,39 @@
 import requests
-import random
 import chess
-import time
+import chess.polyglot
+import chess.engine
 headers = {"Content-Type": "text/plain"}
 url = "http://localhost/chess/api.php"
+position = "http://localhost/chess/position.txt"
+white = "http://localhost/chess/white.txt"
+black = "http://localhost/chess/black.txt"
+white_assign = "http://localhost/chess/assign_white.php"
+black_assign = "http://localhost/chess/assign_black.php"
+engine_name = ""
 with requests.Session() as session:
-    while True:
-        board = chess.Board()
-        session.post(url, data="start", headers=headers)
-        time.sleep(1)
-        while board.is_game_over(claim_draw=True) == False:
-            board.push(random.choice(list(board.legal_moves)))
-            session.post(url, data=board.fen(), headers=headers)
-            time.sleep(1)
+	while True:
+		while True:
+			fen = requests.get(position).content.decode()
+			if fen == "start":
+				fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+			board = chess.Board(fen=fen)
+			if board.is_game_over(claim_draw=True):
+				break
+			while True:
+				if board.turn == chess.WHITE and requests.get(white).content.decode() == engine_name or board.turn == chess.BLACK and requests.get(black).content.decode() == engine_name:
+					with chess.polyglot.open_reader("") as reader:
+						try:
+							board.push(reader.choice(board))
+						except IndexError:
+							board.push(engine.play().move)
+			session.post(url, data=board.fen(), headers=headers)
+		board = chess.Board()
+		session.post(url, data="start", headers=headers)
+		if requests.get(white).content.decode() == engine_name:
+			opponent = requests.get(black).content.decode()
+			session.post(white_assign, data=opponent, headers=headers)
+			session.post(black_assign, data=engine_name, headers=headers)
+		elif requests.get(black).content.decode() == engine_name:
+			opponent = requests.get(white).content.decode()
+			session.post(white_assign, data=engine_name, headers=headers)
+			session.post(black_assign, data=opponent, headers=headers)
